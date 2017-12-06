373d87723a472c9d50e9de02358803be1771a02e
Magesh <tkmagesh77@gmail.com>
2017-12-06 09:46:16 +0530

    Calculator One - complete solution

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
@@ -13,6 +14,7 @@ import { CalculatorOneComponent } from './calculator/calculator1.component';
     CalculatorOneComponent
   ],
   imports: [
+  	FormsModule,
     BrowserModule
   ],
   providers: [],
--- first-app/src/app/calculator/calculator1.component.ts
+++ first-app/src/app/calculator/calculator1.component.ts
@@ -6,26 +6,19 @@ import { CalculatorModel } from './CalculatorModel';
 	template : `
 		<h1>Calculator-1</h1>
 		<hr>
-		<input type="number" #txtNumber1>
-		<input type="number" #txtNumber2>
-		<input type="button" value="Add" (click)="onAddClick(txtNumber1.value, txtNumber2.value)">
-		<input type="button" value="Subtract">
-		<input type="button" value="Multiply">
-		<input type="button" value="Divide">
-		<div>{{result}}</div>
+		<input type="number" [(ngModel)]="model.n1" >
+		<input type="number" [(ngModel)]="model.n2" >
+		<input type="button" value="Add" (click)="model.add()">
+		<input type="button" value="Subtract" (click)="model.subtract()">
+		<input type="button" value="Multiply" (click)="model.multiply()">
+		<input type="button" value="Divide" (click)="model.divide()">
+		<div>{{model.result}}</div>
 	`
 })
 export class CalculatorOneComponent{
 	model : CalculatorModel;
-	result : number = 0;
+	
 	constructor(){
 		this.model = new CalculatorModel();
-	}
-
-	onAddClick(number1Value, number2Value){
-		this.model.n1 = parseInt(number1Value);
-		this.model.n2 = parseInt(number2Value);
-		this.model.add();
-		this.result = this.model.result;
 	}
 }
